1315be7c3a14f83ec2187dd80223f5c792481687
Carson Page <pagem.carson@gmail.com>
2015-03-01 10:26:52 -0600

    Changed the options to an object. Breaking change.

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ Licence: MIT http://cpage.mit-license.org/
  */
 
 
-module.exports = function(mongoose){
+module.exports = function(options){
 	// Private:
 	var Grid = require('gridfs-stream');
-	Grid.mongo = mongoose.mongo;
-	var conn = mongoose.connection;
+	var conn;
+	if(options.mongoose){
+		Grid.mongo = options.mongoose.mongo;
+		conn = options.mongoose.connection;
+	}else{
+		Grid.mongo = options.mongo;
+		conn = {db:options.db};
+	}
 	// Public:
 	return function(req, res, next){
 		if(conn.readyState == 1){
@@ -20,6 +26,8 @@ module.exports = function(mongoose){
 				req.body = {};
 
 			var files = [];
+			var streams = [];
+			var streams_response = 0;
 
 			var busboy = new (require("busboy"))({headers: req.headers});
 			var gfs = Grid(conn.db);
@@ -29,18 +37,26 @@ module.exports = function(mongoose){
 			busboy.on("file", function(fieldname, file, filename, encoding, mimetype){
 				var writeStream = gfs.createWriteStream({filename:filename});
 				file.pipe(writeStream);
-				writeStream.on("close",function(file){
-					files.push({_id:file._id,filename:file.filename});
-				});
+				streams.push(writeStream);
+				streams_response++;
 			});
 
+			//Parses fields in the normal req.body way.
 			busboy.on("field",function(fieldname, val, fieldnameTruncated, valTruncated){
 				req.body[fieldname] = val;
 			});
 
 			busboy.on('finish',function(){
 				req.body.files = files;
-				next();
+				streams.forEach(function(e,i){
+					e.on("close",function(file){
+						files.push({_id:file._id,filename:file.filename});
+						streams_response--;
+						if(streams_response === 0){
+							next();
+						}
+					});
+				});
 			});
 			req.pipe(busboy);
 		}
